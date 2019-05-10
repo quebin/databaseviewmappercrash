8f8478bc92c0134b886c2ffbcba0b8c4b379ed11
Kevin Elorza <quebineg@icloud.com>
2019-05-10 09:43:42 +0200

    Initial Commit

---
+++ DataMapperTester/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DataMapperTester
+//
+//  Created by Kevin Elorza on 2019-05-10.
+//  Copyright © 2019 Kevin Elorza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
